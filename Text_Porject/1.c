c9653894bb48beac0d18d6a09a60b72acbb2cbe5
yongshunyin664-byte <yongshunyin664@gmail.com>
2026-07-16 18:27:35 +0800

    重新架构项目主体，依托黑马C语言编程进行目录重构

--- Text_Porject/1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-    char str1[6] = {'H', 'e', 'l', 'l', 'o', '\0'};   // 手动写法
-    char str2[7] = "Hello";  
-    printf("str1: %s\n", str1);
-    printf("str2: %s\n", str2);
-    return 0;
-}
